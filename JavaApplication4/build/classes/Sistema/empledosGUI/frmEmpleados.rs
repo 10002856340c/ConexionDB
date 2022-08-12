0241bcd70b6377562e82197c5db002f03d2c7219
1000256340c <cristiangsanchez928@gmail.com>
2022-08-12 17:31:51 -0500

    Agregando nuestro datos a la interfaz

--- JavaApplication4/build/classes/Sistema/empledosGUI/frmEmpleados.rs
+++
@@ -1,7 +0,0 @@
-sistema.empledosGUI.frmEmpleados$2
-sistema.empledosGUI.frmEmpleados$1
-sistema.empledosGUI.frmEmpleados$4
-sistema.empledosGUI.frmEmpleados$3
-sistema.empledosGUI.frmEmpleados$6
-sistema.empledosGUI.frmEmpleados
-sistema.empledosGUI.frmEmpleados$5
